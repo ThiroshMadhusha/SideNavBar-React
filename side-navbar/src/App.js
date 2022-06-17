e29ab56f1ce864c5706bde5b64ff4757f0b67d9e
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-17 23:38:54 +0530

    Complete App.JS

--- side-navbar/src/App.js
+++ side-navbar/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import SideBar from "./components/SideBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Dashboard from './pages/Dashboard.jsx'
+import About from "./pages/About.jsx";
+import Inventory from "./pages/Inventory.jsx";
+import Supplier from "./pages/Supplier.jsx";
+import Transport from "./pages/Transport.jsx";
+import Contact from "./pages/Contact.jsx";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <SideBar>
+        <Routes>
+          <Route path="/" elements={<Dashboard />} />
+          <Route path="/dashboard" elements={<Dashboard />} />
+          <Route path="/about" elements={<About />} />
+          <Route path="/inventory" elements={<Inventory />} />
+          <Route path="/supplier" elements={<Supplier />} />
+          <Route path="/transport" elements={<Transport />} />
+          <Route path="/contact" elements={<Contact />} />
+        </Routes>
+      </SideBar>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
