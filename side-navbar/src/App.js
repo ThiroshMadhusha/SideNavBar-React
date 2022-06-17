b132e1eb9b71a5542e472e252621e412eac50e80
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-17 21:31:14 +0530

    Create app.js File

--- side-navbar/src/App.js
+++ side-navbar/src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      Hello
+    <div>
+      <h1>Hello World</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
